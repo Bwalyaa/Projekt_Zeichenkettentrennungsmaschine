1a7bba0771d94bea6cc9ac5317c3756e719743e4
Bwalyaa <bwalya@hotmail.de>
2023-12-14 16:32:25 +0100

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,36 @@
 
+// const searchSeparation = () => {
+//     const inputText = document.querySelector("#inputText").value;
+//     const inputTextTwo = document.querySelector("#inputText2").value;
+//     const before = document.querySelector("#davor").checked;
+//     const thereAfter = document.querySelector("#danach").checked;
+//     const resultFront = document.querySelector(".resultFront");
+//     const resultEnd = document.querySelector(".resultEnd");
+
+//     const index = inputText.indexOf(inputTextTwo);
+    
+//     if (index >= 0) {
+//         let textBefore, textThereAfter;
+
+//         if (before) {
+//             textBefore = inputText.substring(0, index);
+//             textThereAfter = inputText.substring(index);
+//         } else if (thereAfter) {
+//             const searchTextIndex = inputText.indexOf(inputTextTwo);
+            
+//             if (searchTextIndex >= 0) {
+//                 textBefore = inputText.substring(0, searchTextIndex + inputTextTwo.length);
+//                 textThereAfter = inputText.substring(searchTextIndex + inputTextTwo.length);
+//             } else {
+//                 resultFront.innerHTML = "not found"
+//             }
+//         }
+
+//         resultFront.textContent = textBefore;
+//         resultEnd.textContent = textThereAfter;
+//     } 
+// }
+
 const searchSeparation = () => {
     const inputText = document.querySelector("#inputText").value;
     const inputTextTwo = document.querySelector("#inputText2").value;
@@ -8,7 +40,7 @@ const searchSeparation = () => {
     const resultEnd = document.querySelector(".resultEnd");
 
     const index = inputText.indexOf(inputTextTwo);
-    
+
     if (index >= 0) {
         let textBefore, textThereAfter;
 
@@ -17,17 +49,14 @@ const searchSeparation = () => {
             textThereAfter = inputText.substring(index);
         } else if (thereAfter) {
             const searchTextIndex = inputText.indexOf(inputTextTwo);
-            
+
             if (searchTextIndex >= 0) {
                 textBefore = inputText.substring(0, searchTextIndex + inputTextTwo.length);
                 textThereAfter = inputText.substring(searchTextIndex + inputTextTwo.length);
-            } else {
-                resultFront.innerHTML = "not found"
             }
         }
-
         resultFront.textContent = textBefore;
         resultEnd.textContent = textThereAfter;
-    } 
+    }
 }
 
