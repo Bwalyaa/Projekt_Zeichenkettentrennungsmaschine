be0f54adaf8fe975b49672ae14aeb672b92fa9e2
Bwalyaa <bwalya@hotmail.de>
2023-12-15 08:48:05 +0100

    code optimiert

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,6 @@ const searchSeparation = () => {
     const resultEnd = document.querySelector(".resultEnd");
 
     const index = inputText.indexOf(inputTextTwo);
-    const searchTextIndex = inputText.indexOf(inputTextTwo);
 
     if (index >= 0) {
         let textBefore, textThereAfter;
@@ -17,14 +16,10 @@ const searchSeparation = () => {
             textBefore = inputText.substring(0, index);
             textThereAfter = inputText.substring(index);
         } else if (thereAfter) {
-
-            if (searchTextIndex >= 0) {
-                textBefore = inputText.substring(0, searchTextIndex + inputTextTwo.length);
-                textThereAfter = inputText.substring(searchTextIndex + inputTextTwo.length);
-            }
+            textBefore = inputText.substring(0, index + inputTextTwo.length);
+            textThereAfter = inputText.substring(index + inputTextTwo.length);
         }
         resultFront.innerHTML = textBefore;
         resultEnd.innerHTML = textThereAfter;
     }
 }
-
